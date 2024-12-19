1924566a5e26700b294fc204e573b5b4e0e55c49
Артур Низамитдинов <anezamus-10@mail.ru>
2024-12-19 13:33:25 +0300

    1. Unite two pages .html in one index .html 2. Add functionality that can handle multiple file instef of one

--- cmd/main.go
+++ cmd/main.go
@@ -1,63 +1,102 @@
 package main
 
 import (
-    "fmt"
-    "io"
-    "log"
-    "net/http"
-    "os"
-    "html/template"
+	"fmt"
+	"html/template"
+	"io"
+	"log"
+	"net/http"
+	"os"
 )
 
 func main() {
-    // Обрабатываем маршрут для загрузки файлов
-    http.HandleFunc("/upload", uploadHandler)
+	// Обрабатываем маршрут для загрузки файлов
+    http.HandleFunc("/", index)
+	http.HandleFunc("/upload", uploadHandler)
+	http.HandleFunc("/uploadWithDrop", uploadWithDropHandler)
 
-    // Запускаем сервер на порту 8080
-    fmt.Println("Сервер запущен на :8080")
-    err := http.ListenAndServe(":8080", nil)
+	// Запускаем сервер на порту 8080
+	fmt.Println("Сервер запущен на :8080")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println("Ошибка:", err)
+	}
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+    // Отправляем HTML-форму для загрузки файла
+    w.Header().Set("Content-Type", "text/html; charset=utf-8")
+    template, err := template.ParseFiles("templates/index.html")
     if err != nil {
-        fmt.Println("Ошибка:", err)
+        err = fmt.Errorf("Ошибка при парсинге шаблона %s", "index.html")
+        log.Printf("Ошибка при парсинге шаблона %v", err)
+        return
     }
+    template.Execute(w, "")
+}
+
+func uploadHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		// Получаем все файлы
+        r.ParseMultipartForm(10000)
+		files := r.MultipartForm.File["files"]
+		for _, fileHeader := range files {
+			// Открываем файл
+			file, err := fileHeader.Open()
+			if err != nil {
+				http.Error(w, "Ошибка при открытии файла", http.StatusBadRequest)
+				return
+			}
+			defer file.Close()
+
+			// Создаем файл на сервере
+			dst, err := os.Create(fileHeader.Filename)
+			if err != nil {
+				http.Error(w, "Ошибка при создании файла", http.StatusInternalServerError)
+				return
+			}
+			defer dst.Close()
+
+			// Копируем содержимое загружаемого файла
+			if _, err := io.Copy(dst, file); err != nil {
+				http.Error(w, "Ошибка при записи файла", http.StatusInternalServerError)
+				return
+			}
+			fmt.Fprintf(w, "Файл %s успешно загружен.<br>", fileHeader.Filename)
+		}
+	}
 }
 
 // uploadHandler обрабатывает загрузку файла
-func uploadHandler(w http.ResponseWriter, r *http.Request) {
-    // Проверка метода запроса
-    if r.Method == http.MethodPost {
-        // Получаем файл из формы
-        file, header, err := r.FormFile("file")
-        if err != nil {
-            http.Error(w, "Ошибка при получении файла", http.StatusBadRequest)
-            return
-        }
-        defer file.Close()
+func uploadWithDropHandler(w http.ResponseWriter, r *http.Request) {
+	// Проверка метода запроса
+	if r.Method == http.MethodPost {
+		// Получаем все файлы
+        r.ParseMultipartForm(10000)
+		files := r.MultipartForm.File["filesDragAnDrope"]
+		for _, fileHeader := range files {
+			// Открываем файл
+			file, err := fileHeader.Open()
+			if err != nil {
+				http.Error(w, "Ошибка при открытии файла", http.StatusBadRequest)
+				return
+			}
+			defer file.Close()
 
-        // Создаем файл на сервере
-        dst, err := os.Create(header.Filename)
-        if err != nil {
-            http.Error(w, "Ошибка при создании файла", http.StatusInternalServerError)
-            return
-        }
-        defer dst.Close()
+			// Создаем файл на сервере
+			dst, err := os.Create(fileHeader.Filename)
+			if err != nil {
+				http.Error(w, "Ошибка при создании файла", http.StatusInternalServerError)
+				return
+			}
+			defer dst.Close()
 
-        // Копируем содержимое загружаемого файла
-        if _, err := io.Copy(dst, file); err != nil {
-            http.Error(w, "Ошибка при записи файла", http.StatusInternalServerError)
-            return
-        }
-
-        // Успешное сообщение
-        fmt.Fprintln(w, "Файл успешно загружен.")
-    } else {
-        // Отправляем HTML-форму для загрузки файла
-        w.Header().Set("Content-Type", "text/html; charset=utf-8")
-        template, err := template.ParseFiles("templates/formForDownload.html")
-        if err != nil {
-            err = fmt.Errorf("Ошибка при парсинге шаблона %s", "templates/formForDownload.html")
-            log.Printf("Ошибка при парсинге шаблона %v", err)
-            return
-        }
-        template.Execute(w, "")
-    }
+			// Копируем содержимое загружаемого файла
+			if _, err := io.Copy(dst, file); err != nil {
+				http.Error(w, "Ошибка при записи файла", http.StatusInternalServerError)
+				return
+			}
+			fmt.Fprintf(w, "Файл %s успешно загружен.<br>", fileHeader.Filename)
+		}
+	} 
 }
